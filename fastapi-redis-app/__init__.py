0240babd8aae6311adf9b1971f3269302f679630
Miika Tuominen <miika.km.tuominen@gmail.com>
2025-02-02 20:48:03 +0200

    first commit

---
+++ fastapi-redis-app/__init__.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import redis
+
+app = FastAPI()
+redis_client = redis.Redis(host="redis", port=6379, decode_responses=True)
+
+@app.get("/")
+def read_root():
+    redis_client.incr("visits")
+    visits = redis_client.get("visits")
+    return {"message": "Hii, FastAPI with Redis!", "visits": visits}
+
